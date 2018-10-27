3231dfdfb22f67ee45b249396463d1b26cdadc8a
merhawi <merhawi.temnewo88@gmail.com>
2018-10-27 13:42:06 -0500

    added firebase

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Component} from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  items: any[];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+  constructor( db: AngularFireDatabase) {
+      db.list('portfolio').valueChanges()
+      .subscribe(items =>
+        this.items = items )
+    }
+   }
